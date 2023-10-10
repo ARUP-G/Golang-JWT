2e61964d5c6796a3459151ec2161e82335d15696
Arup <darup2019@gmail.com>
2023-10-10 20:29:07 +0530

    database Connectino file added
    closes #4

---
+++ database/databaseConnection.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	MongoDb := os.Getenv("MONGODB_URL")
+
+	//Connection time
+	ctx, cancle := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancle()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+
+	return client
+
+}

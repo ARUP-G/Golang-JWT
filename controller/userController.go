6620ea5c0da8f818fc00267cf5a6a0d143a7b428
Arup <darup2019@gmail.com>
2023-11-22 20:41:54 +0530

    userController file updated
    user collection and validator added

--- controller/userController.go
+++ controller/userController.go
@@ -1,5 +1,14 @@
 package controller
 
+import (
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+var validate = validator.New()
+
 func HashPassword() {
 
 }
@@ -13,5 +22,11 @@ func Singup() {
 }
 
 func Login() {
+
+}
+func GetUser() {
+
+}
+func GetUsers() {
 
 }
